334eead07c3c9540b199115251519ed0ba6088f2
Suleman23 <109822828+Suleman23@users.noreply.github.com>
2025-04-14 02:14:18 +0500

    Complete exercise #02 repeatString
    Practice writing loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let h = '';
+    if(num >= 0)
+    {
+        for (let i = 1; i <= num; i++) h += str;
+        return h;
+    }else{
+        return 'ERROR';
+    }
+    
+    
+    
 };
 
 // Do not edit below this line
