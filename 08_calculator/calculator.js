1cdb0493697202ee97733f72f7bc7090695540a0
Suleman23 <109822828+Suleman23@users.noreply.github.com>
2025-04-16 22:17:33 +0500

    Complete exercise #08 calculator
    Write functions to add, subtract, sum, multiply,
    find power, find factorial of numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let sum = 0;
+  for (let i = 0; i < num.length; i++) sum += num[i];
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let mul = 1;
+  for (let i = 0; i < num.length; i++) mul *= num[i];
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+
+  let factorial = 1;
+  for (let i = 1; i < num - 1; i++) factorial *= num - i;
+  return num * factorial;
 };
 
 // Do not edit below this line
