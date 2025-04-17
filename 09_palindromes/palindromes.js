dd9646b8a1c2cd942e9188ef3fce0fe9e831efe7
Suleman23 <109822828+Suleman23@users.noreply.github.com>
2025-04-18 00:01:36 +0500

    Complete exercise #09 palindromes
    I copied the part of solution from DeepSeek AI

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let p = "";
+    let s = str;
+    //copied the replace function from DeepSeek AI
+    s = s.toLowerCase().replace(/[\p{P}\s]/gu, "");
+    console.log(s);
+    for (let i = s.length - 1; i >= 0; i--) p += s[i];
 
+    if (p === s) return true;
+    else return false;
 };
 
 // Do not edit below this line
