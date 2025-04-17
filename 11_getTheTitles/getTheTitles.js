77d95ade481ff4412d9f5386037ed0c112878171
Suleman23 <109822828+Suleman23@users.noreply.github.com>
2025-04-18 01:17:29 +0500

    Complete exercise #11 getTheTitles
    Write a function that takes an array of objects
    and returns an array of specific object attributes

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+    let b = [];
+    for (let i = 0; i < book.length; i++) 
+        b[i] = book[i].title;
+    return b;
 };
 
 // Do not edit below this line
