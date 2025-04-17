d074052044d9c3bf12712539bd75158d78b3929f
Suleman23 <109822828+Suleman23@users.noreply.github.com>
2025-04-18 00:58:12 +0500

    Complete exercise #10 fibonacci
    Write a function that return a specific member of
    the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let f = [0];
+    f[0] = 0;
+    f[1] = 1;
 
+    for (let i = 2; i <= n; i++) 
+        f[i] = f[i - 1] + f[i - 2];
+
+    return f[n];
 };
 
 // Do not edit below this line
